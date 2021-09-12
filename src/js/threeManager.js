594fdd3b266afd99d04c72c2cb5c84bbaa8d8c53
Dmitry-Sm <smirnov.dmitry.a@mail.ru>
2021-09-13 00:17:18 +0300

    added resize action

--- src/js/threeManager.js
+++ src/js/threeManager.js
@@ -11,8 +11,9 @@ export class ThreeManager {
         this.scene.background = new THREE.Color(0xcccccc)
 
         const width = window.innerWidth, height = window.innerHeight;
-        this.camera = new THREE.OrthographicCamera( width / - 2, width / 2, height / 2, 
-                                                    height / - 2, 1, 1000 );
+        this.camera = new THREE.OrthographicCamera( -width / 2, width / 2, height / 2, 
+                                                    -height / 2, 1, 1000 );
+
         this.scene.add( this.camera );
         this.camera.position.set( 0, 0, 1000 );
         this.camera.lookAt( 0, 0, 0 );
@@ -23,8 +24,22 @@ export class ThreeManager {
         });
         
         this.renderer.setPixelRatio( window.devicePixelRatio );
-        this.renderer.setSize( width, height );
+
+        window.addEventListener('resize', () => {this.onWindowResize()} );
+        this.onWindowResize();
     }
+
+    onWindowResize() {
+        const width = window.innerWidth, height = window.innerHeight;
+
+        this.camera.left =  -width / 2;
+        this.camera.right =  width / 2;
+        this.camera.top =  height / 2;
+        this.camera.bottom =  -height / 2;
+
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize( width, height );
+    };
 
     render() {
         this.renderer.render( this.scene, this.camera );
